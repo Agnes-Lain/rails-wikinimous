d7a85fe26ecd7364b580cf4c2d1228867e88bba7
Yizhu ZONG <agnes.lain@gmail.com>
2020-08-11 16:55:03 +0200

    adding bootstrap

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,6 @@
 class ArticlesController < ApplicationController
+  before_action :set_article, only: [:show, :edit, :update, :destroy]
+
   def index
     @articles = Article.all
   end
@@ -14,24 +16,19 @@ class ArticlesController < ApplicationController
   end
 
   def edit
-    @article = Article.find(params[:id])
   end
 
   def show
-    @article = Article.find(params[:id])
   end
 
   def update
-    @article = Article.find(params[:id])
     @article.update(article_params)
 
     redirect_to article_path(@article)
   end
 
   def destroy
-    @article = Article.find(params[:id])
     @article.destroy
-
     redirect_to articles_path
   end
 
@@ -39,5 +36,9 @@ class ArticlesController < ApplicationController
 
   def article_params
     params.require(:article).permit(:title, :content)
+  end
+
+  def set_article
+    @article = Article.find(params[:id])
   end
 end
